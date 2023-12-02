2584cc27bb93fa646a0ab879255e67338494769e
Cardmore Chiro <cardmore.chiro@affiliate.imd.org>
2023-12-02 15:54:54 +0200

    Cardmore: Domain Changes

---
+++ Doctorly.Domain/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doctorly.Domain
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int EventAppointmentID { get; set; }
+        public int DoctorID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TimeSpan Duration  { get; set; }   
+        public string Comments { get; set; }
+    }
+}
---
+++ Doctorly.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Doctorly.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
